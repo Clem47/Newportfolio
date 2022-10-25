37671ee78f9171fa4802c6b22aec218a2512a7f0
ccrispel <clement.crispel@gmail.com>
2022-10-25 19:01:48 +0200

    avancement js

--- JS/project.js
+++ JS/project.js
@@ -1,5 +1,8 @@
 const git = document.querySelector(".git");
+const trait = document.querySelector("nav div")
+const linkdin = document.querySelector(".linkedin")
 const menu = document.querySelector(".menu a");
+const mail = document.querySelector(".mail")
 const haut = document.querySelector(".title");
 
 window.addEventListener('scroll',()=>{
@@ -8,5 +11,26 @@ window.addEventListener('scroll',()=>{
     }
     else{
         git.src = "./picture/git.png";
+    }
+
+    if (window.scrollY>haut.clientHeight*1.2){
+        linkdin.src = "./picture/linkedinb.png";
+    }
+    else{
+        linkdin.src = "./picture/linkedin.png";
+    }
+
+    if (window.scrollY>haut.clientHeight*1.32){
+        mail.src = "./picture/mailb.png";
+    }
+    else{
+        mail.src = "./picture/mail.png";
+    }
+
+    if (window.scrollY>haut.clientHeight*1.2){
+        trait.src = "./picture/linkedinb.png";
+    }
+    else{
+        //trait.style.backgroundColor = 
     }
 })

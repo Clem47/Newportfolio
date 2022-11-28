236ee5eb51bed82384ff931fe9482fc379f8cc44
Crispel Clément <clement.crispel@gmail.com>
2022-11-28 11:04:19 +0100

    new portfolio

--- JS/project.js
+++ JS/project.js
@@ -6,31 +6,24 @@ const mail = document.querySelector(".mail")
 const haut = document.querySelector(".title");
 
 window.addEventListener('scroll',()=>{
-    if (window.scrollY>haut.clientHeight*1.285){
+    if (window.scrollY>haut.clientHeight*1.285 && window.scrollY<haut.clientHeight*2.285){
         git.src = "./picture/gitb.png";
     }
     else{
         git.src = "./picture/git.png";
     }
 
-    if (window.scrollY>haut.clientHeight*1.2){
+    if (window.scrollY>haut.clientHeight*1.2 && window.scrollY<haut.clientHeight*2.2){
         linkdin.src = "./picture/linkedinb.png";
     }
     else{
         linkdin.src = "./picture/linkedin.png";
     }
 
-    if (window.scrollY>haut.clientHeight*1.32){
+    if (window.scrollY>haut.clientHeight*1.32 && window.scrollY<haut.clientHeight*2.35){
         mail.src = "./picture/mailb.png";
     }
     else{
         mail.src = "./picture/mail.png";
-    }
-
-    if (window.scrollY>haut.clientHeight*1.2){
-        trait.src = "./picture/linkedinb.png";
-    }
-    else{
-        //trait.style.backgroundColor = 
     }
 })
